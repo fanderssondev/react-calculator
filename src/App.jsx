2fe19523d7cda997a57950136e4ff3f96d001c73
nanobyter <sotarfredrik@gmail.com>
2023-04-05 23:21:49 +0200

    Basic app finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import DigitButton from './DigitButton';
 import OperationButton from './OperationButton';
 import './styles.css';
@@ -14,16 +14,143 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
       if (payload.digit === '0' && state.currentOperand === '0') return state;
-      if (payload.digit === '.' && state.currentOperand.includes('.'))
+
+      if (payload.digit === '.' && !state.currentOperand) {
+        return {
+          ...state,
+          currentOperand: '0.',
+        };
+      }
+      if (payload.digit === '.' && state.currentOperand.includes('.')) {
         return state;
-      //BUG Error when starting with a .
-      //BUG Leading 0 not deleted when adding new digit
+      }
+
       return {
         ...state,
         currentOperand: `${state.currentOperand || ''}${payload.digit}`,
       };
+
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+
+    case ACTIONS.CLEAR:
+      return {};
+    // NOTE To show a 0 instead of nothing
+    // return {
+    //   ...state,
+    //   currentOperand: '0',
+    //   previousOperand: null,
+    //   operation: null,
+    // };
+
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: null,
+        };
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+
+    case ACTIONS.EVALUATE:
+      if (
+        state.currentOperand == null ||
+        state.previousOperand == null ||
+        state.operation == null
+      ) {
+        return state;
+      }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      };
   }
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const previous = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+
+  if (isNaN(previous) || isNaN(current)) return '';
+
+  let result = '';
+  switch (operation) {
+    case '+':
+      result = previous + current;
+      break;
+    case '-':
+      result = previous - current;
+      break;
+    case '*':
+      result = previous * current;
+      break;
+    case '÷':
+      result = previous / current;
+      break;
+  }
+  return result.toString();
+}
+
+const INTEGER_FORMATTER = new Intl.NumberFormat('en-us', {
+  maximumFractionDigits: 0,
+});
+
+function formatOperand(operand) {
+  if (operand == null) return;
+  const [integer, decimal] = operand.split('.');
+  if (decimal == null) {
+    return INTEGER_FORMATTER.format(integer);
+  }
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
 }
 
 function App() {
@@ -32,18 +159,40 @@ function App() {
     {}
   );
 
+  // NOTE Keyboard functionality currently not working
+  // useEffect(() => {
+  //   document.addEventListener('keydown', detectKeyDown);
+  // }, []);
+
+  // function detectKeyDown(e) {
+  //   console.log(e.key);
+  //   const key = 1;
+  //   dispatch({ type: ACTIONS.ADD_DIGIT, payload: e.key.toString() });
+  // }
+
   return (
     <div className='calculator-grid'>
       <div className='output'>
         <div className='previous-operand'>
-          {previousOperand} {operation}
+          {formatOperand(previousOperand)} {operation}
         </div>
-        <div className='current-operand'>{currentOperand}</div>
+        <div className='current-operand'>{formatOperand(currentOperand)}</div>
       </div>
-      <button className='span-two'>AC</button>
-      <button>DEL</button>
+      <button
+        className='span-two'
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
+      </button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+        DEL
+      </button>
       <OperationButton operation='÷' dispatch={dispatch} />
-      <DigitButton digit='1' dispatch={dispatch} />
+      <DigitButton
+        digit='1'
+        dispatch={dispatch}
+        onKeyPress={() => console.log('dispatch')}
+      />
       <DigitButton digit='2' dispatch={dispatch} />
       <DigitButton digit='3' dispatch={dispatch} />
       <OperationButton operation='*' dispatch={dispatch} />
@@ -57,7 +206,12 @@ function App() {
       <OperationButton operation='-' dispatch={dispatch} />
       <DigitButton digit='.' dispatch={dispatch} />
       <DigitButton digit='0' dispatch={dispatch} />
-      <button className='span-two'>=</button>
+      <button
+        className='span-two'
+        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+      >
+        =
+      </button>
     </div>
   );
 }
